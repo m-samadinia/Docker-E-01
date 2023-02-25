5525d88d56adb090da020a473d3d698ce635e732
Mehdi Samadinia <m.samadinia@tosan.com>
2023-02-25 10:34:50 +0330

    Add health check service

--- app/routers/api/routes.py
+++ app/routers/api/routes.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter
+from starlette.responses import PlainTextResponse
 
 router = APIRouter()
 
 
-@router.get('/hello')
+@router.get('/helloworld', response_class=PlainTextResponse)
 def hello(name: str = 'Stranger'):
     return f'Hello {name}'
 
 
-@router.get('/author')
+@router.get('/author', response_class=PlainTextResponse)
 def author():
     return 'Mehdi Samadinia'
+
+
+@router.get('/health', response_class=PlainTextResponse)
+async def health():
+    return PlainTextResponse(content="", status_code=200)
